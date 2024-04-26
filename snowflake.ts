a3c50a2c7e25e7d7412b65423a86ca4708707a45
l-m <l-m@l-m.dev>
2024-04-26 23:40:05 +1000

    db: switch to 53 bit snowflake

--- snowflake.ts
+++ snowflake.ts
@@ -1,4 +1,5 @@
-import { NewType } from "./types"
+import { assert } from "./pass_misc"
+import { Snowflake } from "./types"
 
 const epoch = 0 /* +Date.UTC(1970, 0, 1) */
 
@@ -9,14 +10,44 @@ let sequence = 0
 // - https://en.wikipedia.org/wiki/Snowflake_ID
 
 // this isn't a distributed system, so im going to change the internals
-
+//
 //  63 bit strictly positive integer (unsigned)
 //   1 bit + 48 bits + 15 bits = 64 bits
 // +-+----------------------------------------------------+---------------+
 // |0|                     timestamp                      |   sequence    |
 // +-+----------------------------------------------------+---------------+
 
-export type Snowflake = NewType<'Snowflake', bigint>
+// UPDATE: the javascript bigint type isn't DB safe, drizzle+bun:sqlite destroy
+//         it at the API boundary. ive made mass notes but the gist is:
+//         - snowflakes are stored as 63 (64) bit integers inside a JS bigint
+//         - bigint insertion is fine, they're serialised as is
+//         - bigint extraction causes issues, bun:sqlite converts them into
+//           the JS number type, which is a 64 bit floating point double
+//         - past 2^53 (52 bits + 1 bit implied = 53 bits in mantissa)
+//           most integers are not representable, and the precision is lost
+//         the solution?
+//         - using anything other than integers causes overhead and distrupts
+//           (fast) sorting, indexing, and querying
+//         - there isn't a good one, except biting the bullet and using
+//           only integers inside the interval of [0, 2^53]
+
+// in my tests the sequence bit hovered around 200, safe cap at 500
+//
+// >>> # 44 bits + 9 bits = 53 bits
+// >>> 2**44 / 1000 / 60 / 60 / 24 / 365
+// 557.8445600081178     (can store about 557 years in here, which is annoying)
+
+// you know? fuck the sequence bits. we aren't twitter
+
+// im going to opt to use 5 bits of sequence, and use 48 bits of timestamp
+// then just hard block when those 5 bits are exeeded
+// its rare that you'll be generating more than 32 identifiers in the same mili
+
+//  53 bit strictly positive integer (unsigned)
+//   5 bits + 48 bits = 53 bits
+// +------------------------------------------------+-----+
+// |              timestamp  (48 bits)              | seq |
+// +------------------------------------------------+-----+
 
 export function snowflake(): Snowflake {
 	const now = Date.now()
@@ -24,14 +55,17 @@ export function snowflake(): Snowflake {
 	if (last_timestamp !== now) {
 		sequence = 0
 		last_timestamp = now
+	} else if (sequence == 32) {
+		Bun.sleepSync(1)
+		return snowflake()
 	}
 	const seq = sequence++
 
-	const snowflake = BigInt(epoch + now) << 15n | BigInt(seq)
+	const int_snowflake = BigInt(epoch + now) << 5n | BigInt(seq)
 
-	return snowflake as Snowflake
+	return Number(int_snowflake)
 }
 
 export function snowflake_timestamp(snowflake: Snowflake): Date {
-	return new Date(Number(snowflake >> 15n) + epoch)
+	return new Date(Number(BigInt(snowflake) >> 5n) + epoch)
 }
