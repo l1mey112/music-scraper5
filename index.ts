a2cbaafcf6ec16b155a8e45b097c32db1e00debe
l-m <l-m@l-m.dev>
2024-05-13 12:29:12 +1000

    ui: index.ts `ui_show` with open

--- index.ts
+++ index.ts
@@ -1,3 +1,4 @@
+import open from "open"
 import { _manual_cleanup, atexit } from "./atexit"
 import { pass } from "./pass"
 import { seed_root } from './seed'
@@ -9,29 +10,34 @@ if (directive === 'ui') {
 	serve_ui()
 }
 
-try {
-	await seed_root()
-
-	for await (const status of pass()) {
-		switch (status.kind) {
-			case 'before': {
-				process.stdout.write(`\r${status.pass}...`)
-				break
-			}
-			case 'after': {
-				process.stdout.write(`\r${status.pass}... ${Math.round(status.msec)}ms\n`)
-				break
-			}
-			case 'error': {
-				let msg = status.pass ? `\r${status.pass}: error` : 'error:'
-				console.error(msg, status.error)
-				if (status.throwable) {
-					throw status.throwable
+if (directive === 'ui_show') {
+	serve_ui()
+	await open('http://localhost:3000')
+} else {
+	try {
+		await seed_root()
+	
+		for await (const status of pass()) {
+			switch (status.kind) {
+				case 'before': {
+					process.stdout.write(`\r${status.pass}...`)
+					break
+				}
+				case 'after': {
+					process.stdout.write(`\r${status.pass}... ${Math.round(status.msec)}ms\n`)
+					break
+				}
+				case 'error': {
+					let msg = status.pass ? `\r${status.pass}: error` : 'error:'
+					console.error(msg, status.error)
+					if (status.throwable) {
+						throw status.throwable
+					}
 				}
 			}
 		}
+	} catch (e) {
+		console.error(e)
+		_manual_cleanup() // TODO: bun doesn't support process.on('uncaughtException', ...)
 	}
-} catch (e) {
-	console.error(e)
-	_manual_cleanup() // TODO: bun doesn't support process.on('uncaughtException', ...)
 }
